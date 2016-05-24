0467eb626b136a20d433a80d74ae7cd507c12cc8
harrisonlingren <harrisonlingren@gmail.com>
2016-05-24 16:17:42 -0400

    fixed index

--- web/app/js/services.js
+++ web/app/js/services.js
@@ -1,10 +1,11 @@
-angular.module('TasksApp.services', []).factory('firebaseAPI', function($http) {
-    var queryResults = {};
-    firebaseAPI.getTasks = function() {
-      return $http({
-        method: 'JSONP', 
-        url: 'https://tasks-7f318.firebaseio.com/tasks.json'
-      });
-    }
-    return queryResults;
+angular.module('TasksApp.services', []).
+	service('firebaseAPI', function($http) {
+		var queryResults = {};
+		firebaseAPI.getTasks = function() {
+		  return $http({
+			method: 'JSONP', 
+			url: 'https://tasks-7f318.firebaseio.com/tasks.json'
+		  });
+		}
+		return queryResults;
   });
